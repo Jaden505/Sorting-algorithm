e78c7a040fb9a686cae291831721995068de7fb5
@rijswij2 <jaden.van.rijswijk@hva.nl>
2022-07-20 11:01:49 +0200

    Beginning on Sorting and classified main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-let disp = new Display();
-let rand = new Randomize();
+class Main {
+    constructor() {
+        // Init classes
+        self.disp = new Display();
+        self.rand = new Randomize();
+        self.sorting = new Sort();
 
-disp.createStrokes(50)
+        self.amount_strokes = 50;
+    }
 
-let randomize_button = document.getElementById('randomize');
+    Listeners() {
+        // Buttons
+        let randomize_button = document.getElementById('randomize');
 
-randomize_button.addEventListener('click', function() {
-    rand.Randomize();
-});
+        // Listeners for button click
+        randomize_button.addEventListener('click', function() {
+            rand.Randomize();
+        });
+    }
+}
+
+const m = new Main();
+
+m.Listeners()
+
+m.disp.createStrokes(amount_strokes)
+
+m.sorting.quickSort()
