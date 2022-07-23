921eb37b8b8196124a9125ca6288beea6f3af639
@rijswij2 <jaden.van.rijswijk@hva.nl>
2022-07-23 12:08:03 +0200

    Updates slider heights to screen distance

--- js/utils.js
+++ js/utils.js
@@ -20,8 +20,15 @@ class Utils {
       output.innerHTML = this.value;
       utils.amount_strokes = this.value;
   
+      // Recreate the rects
       rects.innerHTML = '';
       disp.createStrokes(this.value)
+
+      // Update svg heights so it fits the screen
+      let svgs = document.getElementsByClassName('svg_stroke');
+      Array.from(svgs).forEach(svg => {
+        svg.style.height = 110 + (this.value*5);        
+      });
     }
   }
 }
