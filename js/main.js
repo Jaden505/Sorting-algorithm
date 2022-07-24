72983cf2b6fc0c3badc27720adafea7fe8b3f4b2
@rijswij2 <jaden.van.rijswijk@hva.nl>
2022-07-24 12:28:27 +0200

    selection sort added.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ class Main {
         self.rand = new Randomize();
         self.qs  = new QuickSort();
         self.is = new InsertionSort();
+        self.ss = new SelectionSort();
 
         // Call startup functions
         disp.createStrokes(utils.amount_strokes);
@@ -17,6 +18,7 @@ class Main {
         let randomize_button = document.getElementById('randomize');
         let quicksort_button = document.getElementById('quickSort');
         let insertionsort_button = document.getElementById('insertionSort');
+        let selectionsort_button = document.getElementById('selectionSort');
 
         // Listeners for button click
         randomize_button.addEventListener('click', function() {
@@ -28,13 +30,19 @@ class Main {
         quicksort_button.addEventListener('click', async function() {
             await disp.Analyse();
             await qs.Sort(qs.getHeights());
-            // await disp.Sorted();
+            await disp.Sorted();
         });
 
         insertionsort_button.addEventListener('click', async function() {
             await disp.Analyse();
             await is.Sort(is.getHeights());
-            // await disp.Sorted();
+            await disp.Sorted();
+        });
+
+        selectionsort_button.addEventListener('click', async function() {
+            await disp.Analyse();
+            await ss.Sort(ss.getHeights());
+            await disp.Sorted();
         });
     }
 }
